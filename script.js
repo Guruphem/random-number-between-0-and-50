513c2bc972f80b9591056b9c72392b6346adaa10
Guruphem <leoguruphem@gmail.com>
2023-12-23 03:45:42 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/* 
+CHALLENGE:
+
+Write a javascript code to randomize number within the range of 0 to 50
+*/
+
+function radomizedNum(minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+}
+console.log(radomizedNum(10, 50));
